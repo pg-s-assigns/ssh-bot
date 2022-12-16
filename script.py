41c24f2a0cf29a699d1fd31a60fcda06d9e28575
miska924 <miska924code@gmail.com>
2022-12-16 15:09:40 +0300

    final fixes

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ import argparse
 import threading
 import time
 import subprocess
-import datetime
 
 MAX_TG_MESSAGE_LENGTH = 4096
 SLEEP_AFTER_READ = 0.5
@@ -31,22 +30,21 @@ class SubprocessOutputHandler(threading.Thread):
         self.__chat_id = chat_id
         self.__output_stream = output_stream
 
-    
     def run(self):
         while True:
-            readen_output = self.__output_stream.read1(MAX_TG_MESSAGE_LENGTH).decode(OUTPUT_ENCODING)
+            readen_output = self.__output_stream.read1(
+                MAX_TG_MESSAGE_LENGTH).decode(OUTPUT_ENCODING)
             time.sleep(SLEEP_AFTER_READ)
             bot.send_message(self.__chat_id, readen_output)
 
 
-
 class Subprocess:
     def __init__(self, chat_id):
-        self.__chat_id = chat_id
-        self.__process = subprocess.Popen(['/bin/sh'], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
-        self.__output_handler = SubprocessOutputHandler(chat_id, self.__process.stdout)
+        self.__process = subprocess.Popen(
+            ['/bin/sh'], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.__output_handler = SubprocessOutputHandler(
+            chat_id, self.__process.stdout)
         self.__output_handler.start()
-
 
     def add_symbols(self, symbols):
         self.__process.stdin.write(bytes(symbols, OUTPUT_ENCODING))
@@ -59,21 +57,23 @@ def whitelisted(chat_id=None):
 
 @bot.message_handler(commands=['start', 'help', 'chatid', 'info'])
 def info(message):
-    logging.info('/info from {} {}'.format(message.from_user.username, message.chat.id))
+    logging.info(
+        '/info from {} {}'.format(message.from_user.username, message.chat.id))
     bot.reply_to(message, "ChatID: {}".format(message.chat.id))
 
 
 @bot.message_handler(commands=['exec'])
 def exec(message):
-    logging.info('/exec from {} {}'.format(message.from_user.username, message.chat.id))
+    logging.info(
+        '/exec from {} {}'.format(message.from_user.username, message.chat.id))
     if not whitelisted(message.chat.id):
         return
 
     if message.chat.id not in chat_to_subprocess:
         chat_to_subprocess[message.chat.id] = Subprocess(message.chat.id)
 
-
-    chat_to_subprocess[message.chat.id].add_symbols(message.text[len('/exec '):] + '\n')
+    chat_to_subprocess[message.chat.id].add_symbols(
+        message.text[len('/exec '):] + '\n')
 
 
 if __name__ == '__main__':
